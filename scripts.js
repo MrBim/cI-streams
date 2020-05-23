2b8f061697df5744ecbe4bf0eee06e8a9a85623b
Bim <Bimtech84@gmail.com>
2020-05-23 19:20:08 +0100

    cleaning up master branch

--- scripts.js
+++ scripts.js
@@ -1,19 +1,3 @@
 $(document).ready(() => {
-    const getWords = async () => {
-        const res = await fetch("https://random-word-api.herokuapp.com/word?number=1");
-        if (!res.ok) {
-            throw new Error(`HTTP Error! status: ${res.status}`);
-        } else {
-            return await res.text();
-        }
-    };
-
-    const extractWordString = (arrayString) => {
-        let splitArray = arrayString.split('"');
-        console.log("arrayString: ", splitArray);
-    };
-
-    let words = getWords().then((res) => extractWordString(res));
-
-    console.log("words: ", words);
+    console.log("Ready");
 });
