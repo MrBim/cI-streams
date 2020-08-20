8a86c7fc042dcbaf725ccee430b806410e48aa9e
Bim <Bimtech84@gmail.com>
2020-08-20 20:51:07 +0100

    it kinda works

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
-$(document).ready(() => {
-    console.log("Ready");
-});
+console.log("Ready");
+
+let flippedCards = [];
+
+for (let i = 0; i < 4; i++) {
+    const idString = `card-${i}`;
+    idString;
+    const card = document.getElementById(idString);
+
+    card.addEventListener("click", function (e) {
+        this.classList.toggle("clicked-card");
+
+        if (this.classList.contains("clicked-card")) {
+            flippedCards.push(this.id);
+        } else {
+            const location = flippedCards.indexOf(this.id);
+
+            flippedCards.splice(location, 1);
+        }
+
+        console.log(flippedCards);
+    });
+}
